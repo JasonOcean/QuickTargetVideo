dee8ef60aafd57c5d394e0620a886da8ed074f04
JasonOcean <wangshuntoday@gmail.com>
2016-03-30 02:12:08 +0800

    a

---
+++ QuickTargetVideo/HotVideoTabCell.swift
@@ -0,0 +1,15 @@
+//
+//  HotVideoTabCell.swift
+//  QuickTargetVideo
+//
+//  Created by Jason on 16/3/29.
+//  Copyright © 2016年 Jason. All rights reserved.
+//
+
+import Foundation
+
+class HotVideoTabCell : UITableViewCell {
+    
+    @IBOutlet weak var hotVideoIndex: UILabel!
+    @IBOutlet weak var hotVideoTitle: UILabel!
+}
